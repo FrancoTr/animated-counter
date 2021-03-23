94a7429ddfb80d1c999096efadedfe35f8a8e654
Franco Traverso <franco-t20@outlook.com>
2021-03-23 16:47:23 -0300

    added animated counter script

---
+++ script.js
@@ -0,0 +1,19 @@
+const counters = document.querySelectorAll('.counter'); //Selects all counters, resulting in a notelist (simmilar to an array)
+const speed = 200; //the lower this is, the faster the animation goes
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target')     //The + sign forces the value to be a number
+        const count = +counter.innerText
+
+        const inc = target / speed
+
+        if (count < target) {
+            counter.innerText = count + inc
+            setTimeout(updateCount, 1)  //this function runs the increment until it reachs the respective target
+        } else {
+            count.innerText = target
+        }
+    }
+    updateCount()
+})
